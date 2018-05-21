9c558498b86db20ee88cac97f61a2750024ab8ef
Shinichi Kogai <kogai0121@gmail.com>
2018-05-21 23:14:20 +0900

    Prepare to parse commands

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-extern crate crust;
+// extern crate crust;
 
-use crust::Config;
+#[macro_use]
+extern crate clap;
+
+use clap::{App, Arg, SubCommand};
+// use crust::Config;
 
 /**
  * TODO:
@@ -12,7 +16,43 @@ use crust::Config;
  * init
  */
 
+macro_rules! subcommands {
+  ($name:expr, $description:expr) => {
+    SubCommand::with_name($name).about($description)
+  };
+}
+
 fn main() {
-  let config = Config::default();
-  println!("{:#?}", config);
+  let matches = App::new(crate_name!())
+    .author(crate_authors!("\n"))
+    .about(crate_description!())
+    .version(crate_version!())
+    .subcommands(vec![
+      subcommands!("list", "Show list of peers or data")
+        .arg(Arg::with_name("PEER").short("p").long("peer"))
+        .arg(Arg::with_name("DATA").short("d").long("data")),
+      subcommands!("add", "Add peer or data")
+        .arg(
+          Arg::with_name("PEER")
+            .short("p")
+            .long("peer")
+            .takes_value(true)
+            .multiple(true),
+        )
+        .arg(
+          Arg::with_name("DATA")
+            .short("d")
+            .long("data")
+            .takes_value(true)
+            .multiple(true),
+        ),
+    ])
+    .get_matches();
+
+  match matches.subcommand() {
+    ("list", Some(m)) => {
+      println!("{:?}", m);
+    }
+    _ => unreachable!(),
+  };
 }
